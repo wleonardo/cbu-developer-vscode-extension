c1d8ac902a91b0680b4ccffd57d0752e583dbd55
yuekai.wyk <yuekai.wyk@alibaba-inc.com>
2021-11-09 14:58:13 +0800

    update

--- extension.js
+++ extension.js
@@ -17,69 +17,69 @@ let debug = false
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.info('Congratulations, your extension "cbu-developer-extension" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.info('Congratulations, your extension "cbu-developer-extension" is now active!');
 
-	const mainProcess = async () => {
-		const response = await fetch(
-			!debug
-				? "https://unpkg.alibaba-inc.com/@ali/ks-cbu-developer-vscode-extension/lib/index.js"
-				: `http://npmcache.oss-cn-zhangjiakou.aliyuncs.com/cjs/@ali/ks-cbu-developer-vscode-extension/daily/index.js`
-		);
+  const mainProcess = async () => {
+    const response = await fetch(
+      !debug
+        ? "https://unpkg.alibaba-inc.com/@ali/ks-cbu-developer-vscode-extension/lib/index.js"
+        : `http://npmcache.oss-cn-zhangjiakou.aliyuncs.com/cjs/@ali/ks-cbu-developer-vscode-extension/daily/index.js`
+    );
 
-		const data = await response.text();
+    const data = await response.text();
 
-		eval(data);
-	}
+    eval(data);
+  }
 
-	mainProcess();
+  mainProcess();
 
-	let lastUpate = 0
+  let lastUpate = 0
 
-	const update = _.debounce(async () => {
-		// 如果超过10分钟
-		if (now() - lastUpate > 1 * minute) {
-			try {
-				await vscode.window.removeExtension();
-			} catch (e) { }
-			lastUpate = now();
-			mainProcess()
-		}
-	}, 1000);
+  const update = _.debounce(async () => {
+    // 如果超过10分钟
+    if (now() - lastUpate > 1 * minute) {
+      try {
+        await vscode.window.removeExtension();
+      } catch (e) { }
+      lastUpate = now();
+      mainProcess()
+    }
+  }, 1000);
 
-	// window变化
-	vscode.window.onDidChangeWindowState(d => {
-		update();
-	})
+  // window变化
+  vscode.window.onDidChangeWindowState(d => {
+    update();
+  })
 
-	// 编辑的文件变化
-	vscode.window.onDidChangeActiveTextEditor(d => {
-		update();
-	})
+  // 编辑的文件变化
+  vscode.window.onDidChangeActiveTextEditor(d => {
+    update();
+  })
 
-	// vscode.window.onDidChangeActiveTextEditor((event) => {
-	// 	console.info("------onDidChangeActiveTextEditor ----->");
-	// 	console.info(event);
-	// })
+  // vscode.window.onDidChangeActiveTextEditor((event) => {
+  // 	console.info("------onDidChangeActiveTextEditor ----->");
+  // 	console.info(event);
+  // })
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cbu-developer-extension.cbu-developer-extension-debug', function () {
-		// The code you place here will be executed every time your command is executed
-		debug = !debug;
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from cbu-developer-extension-debug!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('cbu-developer-extension.cbu-developer-extension-debug', function () {
+    // The code you place here will be executed every time your command is executed
+    debug = !debug;
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World from cbu-developer-extension-debug!');
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 function deactivate() { }
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate
 }
